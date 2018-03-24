34e21119efab099941b4949df535a1ab9fcc1aad
Ceaser54 <git@github.com:Ceaser54/Software-phases.git>
2018-03-24 03:55:02 +0200

    first commit

---
+++ app/src/main/java/com/example/hp/videoview/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.hp.videoview;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+
+public class MainActivity extends AppCompatActivity {
+    VideoView videoView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        videoView=(VideoView)findViewById(R.id.v);
+
+        videoView.setVideoURI(Uri.parse("http://www.androidbegin.com/tutorial/AndroidCommercial.3gp"));
+        videoView.setMediaController(new MediaController(this));
+        videoView.requestFocus();
+        videoView.start();
+
+
+
+    }
+}
